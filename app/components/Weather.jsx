5931c7d6b559742523f86b38f062a401787878cc
Alex Efremov <alex.efremov.dev@gmail.com>
2016-11-14 23:26:28 +0200

    Add nav search support

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -16,7 +16,9 @@ var Weather = React.createClass({
 
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
     });
     getTemperature(location).then(function(temp) {
       that.setState({
@@ -31,6 +33,22 @@ var Weather = React.createClass({
       });
     });
 
+  },
+  componentDidMount: function() {
+    var location = this.props.location.query.location;
+
+    if(location && location.length > 0) {
+      this.handleNewLocation(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function(newProps){
+    var location = newProps.location.query.location;
+
+    if(location && location.length > 0) {
+      this.handleNewLocation(location);
+      window.location.hash = '#/';
+    }
   },
   render: function() {
     var {isLoading, temp, location, errorMessage} = this.state;
